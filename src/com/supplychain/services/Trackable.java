03d0c319e9c2f0952ec6d51c6e10125287674f74
r-treasa27 <102158597+r-treasa27@users.noreply.github.com>
2025-04-24 18:12:56 +0530

    Update Trackable.java

--- src/com/supplychain/services/Trackable.java
+++ src/com/supplychain/services/Trackable.java
@@ -1,5 +1,9 @@
 package com.supplychain.services;
 
 public interface Trackable{
-
+  //@param productCode is the unique identifier of the product
+	void trackInventory(String productCode);
+	
+	//param shipmentId is the unique identifier of the shipment
+	void trackShipment(String shipmentId);
 }
