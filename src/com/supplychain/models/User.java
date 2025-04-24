ab40e20970bf6e4ba6639afb222e560d9f91cda9
shashwat1807 <137035394+shashwat1807@users.noreply.github.com>
2025-04-24 15:23:01 +0530

    Updated User.java

--- src/com/supplychain/models/User.java
+++ src/com/supplychain/models/User.java
@@ -1,4 +1,27 @@
 package com.supplychain.models;
+import java.util.*;
 
 public abstract class User {
+  protected Integer id;
+  protected String name;
+  protected String role;
+  protected boolean loggedIn = false;
+
+  public User(Integer id, String name, String role) {
+    this.id = id;
+    this.name = name;
+    this.role = role;
+  }
+  public void login() {
+    loggedIn = true;
+    System.out.println(name + " logged in.");
+  }
+  public void logout() {
+    loggedIn = false;
+    System.out.println(name + " logged out.");
+  }
+
+  public boolean isLoggedIn() {
+    return loggedIn;
+  }
 }
