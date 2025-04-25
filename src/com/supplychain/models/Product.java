5022476a82f79e235192c8f298a01f34d4a938bb
SyedHamadanAhmad <f20212337@goa.bits-pilani.ac.in>
2025-04-25 12:58:44 +0530

    Finished Product.java

--- src/com/supplychain/models/Product.java
+++ src/com/supplychain/models/Product.java
@@ -1,4 +1,30 @@
 package com.supplychain.models;
 
 public class Product {
+    private  String code;
+    private  String name;
+    private  String category="";
+
+    public Product(String code, String name, String category) {
+        this.code = code;
+        this.name = name;
+        this.category = category;
+    }
+
+    public Product(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
 }
